8f5ac394440b446c06d3a725888fd8d22db4216c
NailAbdulganiev <nail_abdulganiev@mail.ru>
2022-03-22 20:27:35 +0400

    11

--- 11.py
+++ 11.py
@@ -1,3 +1,19 @@
 class Summator:
+    def transform(self, n):
+        return n
 
-     
+    def sum(self, n):
+        sum = 0
+        for i in range(n + 1):
+            sum += self.transform(i)
+        return sum
+
+
+class SquareSummator(Summator):
+    def transform(self, n):
+        return n ** 2
+
+
+class CubeSummator(Summator):
+    def transform(self, n):
+        return n ** 3
